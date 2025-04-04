a8c02290cb5897267743011e36019adfbe4ec70b
Heklast <hst10@hi.is>
2025-04-04 20:27:09 +0000

    es

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['picsum.photos', 'loremflickr.com', 'res.cloudinary.com'],
-
+  },
+  eslint: {
+    ignoreDuringBuilds: true, 
   },
 };
 
